c9a55ee9c256a89a406277a5a3b2b62aab48c498
tiahelou10 <tia.elhelou@outlook.com>
2022-03-27 23:19:47 +0300

    Linked Calculator page to the Landing page

--- Front-end/Currency Converter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity2.java
+++ Front-end/Currency Converter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity2.java
@@ -2,7 +2,9 @@ package com.csc498_lau.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void goToMain (View view) {
+
+        Intent obj = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(obj);
     }
 }
