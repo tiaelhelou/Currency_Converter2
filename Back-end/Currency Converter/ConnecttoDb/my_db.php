42c539faca500264eec2c61bb3d4508c1d877021
tiahelou10 <tia.elhelou@outlook.com>
2022-03-28 02:41:26 +0300

    Created a databse on phpMyAdmin and connected the server to it

---
+++ Back-end/Currency Converter/ConnecttoDb/my_db.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_host = "localhost"; 
+$db_user = "root"; 
+$db_password = null; 
+$db_name = "currencydb"; 
+
+
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name); 
+
+
+?>
