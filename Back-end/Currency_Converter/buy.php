ec11d604752b7d5fd047d54a08be3eb60314534a
tiahelou10 <tia.elhelou@outlook.com>
2022-03-31 20:00:14 +0300

    Storing the buy updated rate as an integer in a variable

--- Back-end/Currency_Converter/buy.php
+++ Back-end/Currency_Converter/buy.php
@@ -1,9 +1,9 @@
 <?php
 
-$url = 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233019';
+$url = 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233117';
 $json = file_get_contents($url);
 $jo = json_decode($json);
 $length = count($jo->buy);
-echo $jo->buy[$length - 1][1];
+$updated_buy = intval($jo->buy[$length - 1][1]);
 
 ?>
