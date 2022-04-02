f8987c6435693338ed04281d618d73838c118274
tiahelou10 <tia.elhelou@outlook.com>
2022-04-03 02:00:36 +0300

    Sending the amount entered by the user to the database (back-end)

--- Front-end/Currency Converter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity2.java
+++ Front-end/Currency Converter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity2.java
@@ -3,10 +3,45 @@ package com.csc498_lau.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... arg0) {
+            try {
+                String lbp = (String) arg0[0];
+                String usd = (String) arg0[1];
+
+                String link = "http://192.168.1.7/Currency_Converter/get_amount.php";
+                String data = URLEncoder.encode("lbp", "UTF-8") + "=" +
+                        URLEncoder.encode(lbp, "UTF-8");
+                data += "&" + URLEncoder.encode("usd", "UTF-8") + "=" +
+                        URLEncoder.encode(usd, "UTF-8");
+
+                URL url = new URL(link);
+                URLConnection conn = url.openConnection();
+
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                wr.write(data);
+                wr.flush();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
