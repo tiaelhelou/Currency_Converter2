9bf95dfd8566df8d3d7d1001b9d08eeee232b124
tiahelou10 <tia.elhelou@outlook.com>
2022-04-02 23:49:40 +0300

    Connecting app with the database and getting the updated rates

--- Front-end/Currency Converter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity.java
+++ Front-end/Currency Converter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity.java
@@ -18,11 +18,62 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls){
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+
+            try{
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try{
+                JSONObject json = new JSONObject(s);
+                String buy = json.getString("buy_rate");
+                String sell = json.getString("sell_rate");
+                Log.i("buy_rate", buy);
+                Log.i("sell_rate", sell);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        String url = "http://192.168.1.7/Currency_Converter/get_updated_rate.php";
+
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
 
     }
 
